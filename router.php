4492e36c1b6f532edef3908e4c25c45d02c143ac
Fred ABLEFONLIN <Fred.Ablefonlin@etu.univ-paris1.fr>
2023-12-12 14:49:24 +0100

    version 14h50

--- router.php
+++ router.php
@@ -1,50 +1,35 @@
 <?php
 
-// Liste des pages autorisées et leurs fichiers correspondants
-$pages = [
-    'accueil' => 'accueil.php',
-    'connexion' => 'connexion.php',
-    'inscription' => 'inscription.php',
-    // Ajoutez d'autres pages au besoin
-];
+class Router
+{
+    private $pages;
 
-
-// Récupère le paramètre 'page' depuis l'URL, par exemple, localhost/router.php?page=connexion
-$page = isset($_GET['page']) ? $_GET['page'] : 'accueil';
-
-// Vérifie si la page demandée existe dans la liste, sinon redirige vers la page d'accueil
-if (array_key_exists($page, $pages)) {
-    include($pages[$page]);
-} else {
-    include($pages['accueil']);
-}
-?>
-
-<?php 
-
-
-class Router  {
-
-    public function Roue()
+    public function __construct()
     {
-        return $this->executeQuery("SELECT * FROM `pages`"); 
+    
+        $this->pages = [
+            'accueil' => 'accueil.php',
+            'connexion' => 'connexion.php',
+            'inscription' => 'inscription.php',
+            
+        ];
     }
-        
-    public function calculeSolde()
-    {
-        $depenses = $this->executeQuery("SELECT * FROM `pages`");
-        $total = 0;
 
-        if($depenses){
-            foreach($depenses as $depense){
-                if($depense['debite'] == 1){
-                    $total -= $depense['prix'];
-                }else{
-                    $total += $depense['prix'];
-                }
-            }
+    public function route()
+    {
+        // Récupère le paramètre 'page' depuis l'URL, par exemple
+        $page = isset($_GET['page']) ? $_GET['page'] : 'accueil';
+
+        // Vérifie si la page demandée existe dans la liste, sinon redirige vers la page d'accueil
+        if (array_key_exists($page, $this->pages)) {
+            include($this->pages[$page]);
+        } else {
+            include($this->pages['accueil']);
         }
-        $total = intval($total);
-        return $total;
     }
+}
 
+// Instanciez la classe Router et appelez la méthode route
+$router = new Router();
+$router->route();
+?>
